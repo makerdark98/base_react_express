06bea8ae011636cdc3aa69b4786db074dac54917
Daniel Song <ijacsong98@gmail.com>
2019-01-29 15:04:28 +0900

    Redirect using history
     If user is created as success in signup page and then using history for redirect login.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 /* eslint-env browser */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import history from './history';
+
 
 import './App.css';
 
 
 const App = () => (
-  <div>
-    <Route exact path="/" component={Login} />
-    <Switch>
-      <Route path="/Signup" component={Signup} />
-    </Switch>
-  </div>
+  <Router history={history}>
+    <div>
+      <Route exact path="/" component={Login} />
+      <Switch>
+        <Route path="/Signup" component={Signup} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
